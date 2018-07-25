87030597a64e3199d834c3197e9470cb4765a973
jamesr7946 <jamesrude4@gmail.com>
2018-07-25 15:37:47 -0500

    final game version without speeding up lights

--- Stackers.py
+++ Stackers.py
@@ -16,26 +16,51 @@ class stack():
 		pygame.display.set_mode((640, 480))
 		self.gaming = True
 	def startGame(self):
-		pygame.time.set_timer(USEREVENT +1, 800)
+		pygame.time.set_timer(USEREVENT +1, 170)
 		x=0
+		y =0
+		z=0
 		while self.gaming:
 		
 			for event in pygame.event.get():
 				if event.type == KEYDOWN: 
-					sense.set_pixel(x, y, (0, 0, 255))
-					self.gaming = False	
+					sense.set_pixel(x, y, (255, 20, 255))
+					if y==0:
+						counter = x
+					if y< 8:
+						if x==counter:	
+							y = y+1 	
+							if y==8:
+								pink = (255, 20, 147)
+								blue = (0, 255, 255)
+								speed = 0.2
+								message = 'U won'
+								sense.show_message(message, speed, blue, pink)
+								time.sleep(1)	
+								exit()	
+											
+						else: 
+							pink = (255, 20, 147)
+							blue = (0, 255, 255)
+							speed = 0.2
+							message = 'Loser'
+							sense.show_message(message, speed, blue, pink)
+							time.sleep(1)	
+							exit()	
 				else:				
-					y = 0
 					a = random.randint(0,255)
 					b = random.randint(0,255)
 					c = random.randint(0,255)
 					color = (a, b, c)
 					sense.set_pixel (x, y, color)
-					time.sleep(0.75)
+					time.sleep(0.13)
 					sense.set_pixel(x, y, (0, 0, 0))
+					z=x
 					x = x+1
+					
 					if x==8:
 						x=0
+					
 				
 				
 					
